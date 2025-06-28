7eed3ead3b955c6cb7a5c7f6ad408f3a4e10ac81
karan <karan.matiyali@sportzzbook.com>
2025-06-28 11:01:57 +0530

    input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const core = require('@actions/core');
+
+try {
+  const message = core.getInput('message');
+  console.log(`Message from input: ${message}`);
+  core.setOutput("result", `Printed: ${message}`);
+} catch (error:any) {
+  core.setFailed(`Action failed: ${error.message}`);
+}
